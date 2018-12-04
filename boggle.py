04aa8fc2df122ef0492d5fca01325893f2bf2752
tom1mol <tomasomaolmhuaidh@gmail.com>
2018-12-04 16:52:23 +0000

    neighbouring positions

--- boggle.py
+++ boggle.py
@@ -1,7 +1,55 @@
+from string import ascii_uppercase
+from random import choice       #choice function returns an item from a list at random
+
+
+
 def make_grid(width, height):
     
     #create grid that will hold all of the tiles for a boggle game
     #function creates a dictionary with row/column tuple as the key..and a space as the value
-    return {(row, col): ' ' for row in range(height)
+    return {(row, col): choice(ascii_uppercase)
+        for row in range(height)
         for col in range(width)
 }
+
+
+def neighbours_of_position(coords):
+    
+    #get neighbours of a given position
+    
+    row = coords[0]
+    col = coords[1]
+                                                                    # (-1,-1)  (-1,0)  (-1,1)
+                                                                    # (0, -1)  (0,0)   (0,1)
+                                                                    # (1,-1)  (1,0)   (1,1)
+    #assign each of the neighbours
+    #top left to top right
+    top_left = (row - 1, col - 1)
+    top_center = (row -1, col)
+    top_right = (row - 1, col +1)
+    
+    #left to right
+    left = (row, col - 1)
+    #the (row, col) coordinates passed to this function are situated here
+    right = (row, col + 1)
+    
+    #bottom-left to bottom-right
+    bottom_left = (row + 1, col - 1)
+    bottom_center = (row + 1, col)
+    bottom_right = (row + 1, col + 1)
+    
+    return [top_left, top_center, top_right,
+            left, right,
+            bottom_left, bottom_center, bottom_right]
+    
+def all_grid_neighbours(grid):
+    #get all possible neighbours for each position in the grid
+    
+    neighbours = {}
+    for position in grid:
+        position_neighbours = neighbours_of_position(position)
+        neighbours[position] = [p for p in position_neighbours if p in grid]
+        
+    return neighbours
+    
+        
--- test_boggle.py
+++ test_boggle.py
@@ -3,10 +3,11 @@ import unittest     #unittest framework uses classes and inherritance
                     #methods inside that class for the actual tests
                     
 import boggle   #this is where code we are testing resides(boggle.py)
+from string import ascii_uppercase
 
 class TestBoggle(unittest.TestCase):    #create class inheriting from test case and add docstring to explain purpose
     #test suite for boggle solver
-                                        #test method with docstring giving shot description
+                                        #test method with docstring giving short description
     def test_can_create_an_empty_grid(self): #this case empty grid no assumptions 4 grid shape or data structure used
         #test to see if we can create an empty grid
         grid = boggle.make_grid(0, 0) #height & width or row and column. (0,0) = no rows or columns
@@ -29,4 +30,46 @@ class TestBoggle(unittest.TestCase):    #create class inheriting from test case 
         self.assertIn((1, 0), grid)
         self.assertIn((1, 1), grid)
         self.assertNotIn((2, 2), grid)
+        
+        
+    def test_grid_is_filled_with_letters(self):
+        
+        #ensure each of the coordinates in the grid contains letters
+        
+        grid = boggle.make_grid(2, 3)
+        for letter in grid.values():
+            self.assertIn(letter, ascii_uppercase)
+            
+            
+    
+    def test_neighbours_of_a_position(self):
+        
+        #ensure that a position has 8 neighbours. function takes a position and returns 8 neighbours it should have
+        
+        coords = (1, 2)
+        neighbours = boggle.neighbours_of_position(coords)
+        self.assertIn((0, 1), neighbours)
+        self.assertIn((0, 2), neighbours)
+        self.assertIn((0, 3), neighbours)
+        self.assertIn((1, 1), neighbours)
+        self.assertIn((1, 3), neighbours)
+        self.assertIn((2, 1), neighbours)
+        self.assertIn((2, 2), neighbours)
+        self.assertIn((2, 3), neighbours)
+        
+    def test_all_grid_neighbours(self):
+        #ensure all grid positions have neighbours
+        
+        grid = boggle.make_grid(2, 2)
+        neighbours = boggle.all_grid_neighbours(grid) #get all neighbours 4 grid.
+                                 #is a dictionary where key is position and value is list of neighbouring positions
+        self.assertEqual(len(neighbours), len(grid)) #assert correct length of neighbours dictionary
+        for pos in grid:      #for loop iterates through positions in grid. for each pos neigh are other 3 positions
+            others = list(grid) #creates new list from the dictionaries keys. this is full list
+            others.remove(pos) #minus position in question
+            self.assertListEqual(sorted(neighbours[pos]), sorted(others)) #asserts pos neigh are pos being checked
+            
+        
+        
+        
         
